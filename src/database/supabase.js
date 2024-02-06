cad6ba1c3f4b7dba3dc255705d403af01e31c189
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2024-02-06 12:38:10 -0600

    Init

---
+++ src/database/supabase.js
@@ -0,0 +1,9 @@
+import { createClient } from "@supabase/supabase-js";
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient(
+  "https://wjbhcuqhycnoesmfsfmf.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndqYmhjdXFoeWNub2VzbWZzZm1mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY2MzYyNDYsImV4cCI6MjAyMjIxMjI0Nn0.N8fhRXyeIs1njEu3rEG5hawXmkdgJRctQXfkFXtm6vs"
+);
+
+export default supabase;
