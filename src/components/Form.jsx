7296ed73d8b0a9037311055989db67324a01aa48
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2024-02-13 12:53:20 -0600

    Tarea

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,45 @@
-import React from "react";
+"use client";
+import { supabaseClient } from "app/database/supabase";
+import React, { useEffect, useState } from "react";
 
 const Form = () => {
+  /* Vamos a manejar las variables de nuestro formulario */
+  const [name, setName] = useState("");
+  const [last_name, setLastName] = useState("");
+  const [curp, setCurp] = useState("");
+  const [phone_number, setPhoneNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const [birthdate, setBirthdate] = useState("");
+  const [sex, setSex] = useState("");
+  const [address, setAddress] = useState("");
+
+  /* Creamos funcion para mandar datos del formulario */
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { data, error } = await supabaseClient.from("usuarios").insert([
+      {
+        name,
+        last_name,
+        curp,
+        phone_number,
+        email,
+        birthdate,
+        sex,
+        address,
+      },
+    ]);
+    console.log(data);
+    console.log(error);
+  };
+
+  /*
+  TAREA
+  - Mandar los formuarios a nuestra base de datos
+  - Investigar los tipos de campos del formulario correctos
+  - En la funcion recuperar la información y mandarla a la base de datos
+  - En el campo y el campo dirección, mandar a traer información con useEffect
+  */
+
   return (
     <>
       <form className="form-horizontal">
@@ -44,8 +83,8 @@ const Form = () => {
             </label>
             <div className="col-md-4">
               <input
-                id="curpo"
-                name="curpo"
+                id="curp"
+                name="curp"
                 type="text"
                 placeholder="CURP (18)"
                 className="form-control input-md"
@@ -91,7 +130,7 @@ const Form = () => {
               <input
                 id="birthdate"
                 name="birthdate"
-                type="text"
+                type="date"
                 placeholder="dd/mm/yyyy"
                 className="form-control input-md"
               />
@@ -122,6 +161,7 @@ const Form = () => {
             </div>
           </div>
         </fieldset>
+        <button onClick={handleSubmit}>Enviar</button>
       </form>
     </>
   );
