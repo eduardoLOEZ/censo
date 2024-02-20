3d4b289a83e838739ab5acae88f7d204c8ee8040
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2024-02-20 11:28:18 -0600

    Se agrega comentario

--- src/database/supabase.js
+++ src/database/supabase.js
@@ -2,6 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 // Create a single supabase client for interacting with your database
 export const supabaseClient = createClient(
+  //No olvides modificar esta información
   "https://wjbhcuqhycnoesmfsfmf.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndqYmhjdXFoeWNub2VzbWZzZm1mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY2MzYyNDYsImV4cCI6MjAyMjIxMjI0Nn0.N8fhRXyeIs1njEu3rEG5hawXmkdgJRctQXfkFXtm6vs"
 );
